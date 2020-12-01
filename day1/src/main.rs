01ce48e1ba097b47ec2216fe5f6ae36030739502
Ch4s3r <lamprecht.patrick1@gmail.com>
2020-12-01 20:47:36 +0100

    Add challenge text

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::error::Error;
+use std::time::Instant;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("data/input.txt")?;
@@ -17,15 +18,20 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut count = 0;
 
-    for (index, number1) in numbers.iter().enumerate() {
-        for number2 in &numbers[index + 1..] {
-            count += 1;
-            if number1 + number2 == 2020 {
-                println!("{} * {} = {}", number1, number2, number1 * number2)
+    let start = Instant::now();
+
+    for number1 in &numbers {
+        for number2 in &numbers {
+            for number3 in &numbers {
+                count += 1;
+                if number1 + number2 + number3 == 2020 {
+                    println!("{} * {} * {} = {}", number1, number2, number3, number1 * number2 * number3)
+                }
             }
         }
     }
-
+    let duration = start.elapsed();
+    println!("duration: {:?}", duration);
     println!("iterations: {}", count);
 
     Ok(())
