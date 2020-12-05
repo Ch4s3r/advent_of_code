835f54014f34f64f2904871eb186d0a7a8a20ebc
Ch4s3r <lamprecht.patrick1@gmail.com>
2020-12-05 12:05:54 +0100

    Part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,13 +1,24 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::time::Instant;
 
 const MANDATORY_ATTRIBUTES: [&str; 7] = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+lazy_static! {
+    static ref HGT_REGEX: Regex = Regex::new(r"(\w+)(in|cm)").unwrap();
+    static ref HCL_REGEX: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+    static ref ECL_REGEX: Regex = Regex::new(r"^amb|blu|brn|gry|grn|hzl|oth$").unwrap();
+    static ref PID_REGEX: Regex = Regex::new(r"^\w{9}$").unwrap();
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("data/input.txt")?;
     let reader = BufReader::new(file);
+
+    let start = Instant::now();
 
     let mut concat: Vec<String> = Vec::new();
     let mut passport_strings: Vec<String> = Vec::new();
@@ -21,7 +32,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
     passport_strings.push(concat.join(" "));
-    dbg!(passport_strings.clone());
+    // dbg!(passport_strings.clone());
 
     let mut valid_count = 0;
     for passport in passport_strings.iter() {
@@ -30,20 +41,79 @@ fn main() -> Result<(), Box<dyn Error>> {
             .iter()
             .map(|attribute| {
                 let split_attribute = attribute.split(":").collect::<Vec<&str>>();
-                (split_attribute[0].clone(), split_attribute[1].clone())
+                (split_attribute[0], split_attribute[1])
             })
             .collect::<HashMap<&str, &str>>();
-        let mut valid_passport = true;
+
+        let mut valid_passport_attributes = 0;
         for attribute in MANDATORY_ATTRIBUTES.iter() {
             if !passport_tuples.contains_key(attribute) {
-                valid_passport = false
+                // dbg!(attribute);
+                break;
             }
+            let attribute_value = passport_tuples[attribute];
+            // dbg!(attribute, attribute_value);
+
+            match *attribute {
+                "byr" => {
+                    let year = attribute_value.parse::<i32>().unwrap_or(0);
+                    if (1920..=2020).contains(&year) {
+                        valid_passport_attributes += 1
+                    }
+                }
+                "iyr" => {
+                    let year = attribute_value.parse::<i32>().unwrap_or(0);
+                    if (2010..=2020).contains(&year) {
+                        valid_passport_attributes += 1
+                    }
+                }
+                "eyr" => {
+                    let year = attribute_value.parse::<i32>().unwrap_or(0);
+                    if (2020..=2030).contains(&year) {
+                        valid_passport_attributes += 1
+                    }
+                }
+                "hgt" => {
+                    let capture = match HGT_REGEX.captures(attribute_value) {
+                        Some(x) => x,
+                        _ => break,
+                    };
+                    let unit = capture[2].to_string();
+                    let number = capture[1].parse::<i32>().unwrap();
+                    if unit == "cm" {
+                        if (150..=193).contains(&number) {
+                            valid_passport_attributes += 1
+                        }
+                    } else if unit == "in" {
+                        if (59..=76).contains(&number) {
+                            valid_passport_attributes += 1
+                        }
+                    }
+                }
+                "hcl" => {
+                    if HCL_REGEX.is_match(attribute_value) {
+                        valid_passport_attributes += 1
+                    }
+                }
+                "ecl" => {
+                    if ECL_REGEX.is_match(attribute_value) {
+                        valid_passport_attributes += 1
+                    }
+                }
+                "pid" => {
+                    if PID_REGEX.is_match(attribute_value) {
+                        valid_passport_attributes += 1
+                    }
+                }
+                _ => {}
+            };
         }
-        if valid_passport {
+        if valid_passport_attributes == 7 {
             valid_count += 1;
         }
     }
 
+    dbg!(start.elapsed());
     dbg!(valid_count);
     Ok(())
 }
