c225e4b8ab65f9ae079eff58877ad8f99846f5fe
Ch4s3r <lamprecht.patrick1@gmail.com>
2021-12-01 10:01:17 +0100

    Move and solve day 1 part 1

---
+++ 2021/day1/src/main.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::time::Instant;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("data/input.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut numbers: Vec<i32> = Vec::new();
+
+    for line in reader.lines() {
+        let number = line?.parse()?;
+        numbers.push(number);
+    }
+
+    let mut count = 0;
+
+    for window in numbers.windows(2) {
+        if window[0] < window[1] { count += 1; }
+    }
+    println!("count: {}", count);
+    Ok(())
+}
