fbc96aaa2d425b08f91f1e2b7de142907f16067d
Ch4s3r <lamprecht.patrick1@gmail.com>
2021-12-04 12:28:46 +0100

    Refactor

--- 2021/day3/part2/src/main.rs
+++ 2021/day3/part2/src/main.rs
@@ -1,34 +1,42 @@
-use std::fs::{read_to_string};
+use std::fs::read_to_string;
+
 use anyhow::Context;
+
+use crate::RatingType::{CO2, OXYGEN};
+
+enum RatingType {
+    OXYGEN,
+    CO2,
+}
 
 fn main() -> anyhow::Result<()> {
     let input = read_to_string("data/input.txt")?;
     let lines = input.lines().collect::<Vec<_>>();
 
-    let mut oxygen_input = lines.clone();
-    let mut count = 0;
-    while oxygen_input.len() > 1 {
-        let (ones, zeroes): (Vec<_>, Vec<_>) = oxygen_input.iter().partition(|line| {
-            line.chars().nth(count) == Some('1')
-        });
-        count += 1;
-        oxygen_input = if ones.len() >= zeroes.len() { ones } else { zeroes };
-    }
-    let oxygen_generator_rating = oxygen_input.get(0).context("no element left for oxygen")?;
+    let oxygen_generator_rating = calculate(lines.clone(), OXYGEN)?;
+    let co2_scrubber_rating = calculate(lines.clone(), CO2)?;
 
-    let mut co2_scrubber_input = lines;
-    let mut count = 0;
-    while co2_scrubber_input.len() > 1 {
-        let (ones, zeroes): (Vec<_>, Vec<_>) = co2_scrubber_input.iter().partition(|line| {
-            line.chars().nth(count) == Some('1')
-        });
-        count += 1;
-        co2_scrubber_input = if ones.len() < zeroes.len() { ones } else { zeroes };
-    }
-    let co2_scrubber_rating = co2_scrubber_input.get(0).context("no element left for co2")?;
-
-    let oxygen_int = isize::from_str_radix(oxygen_generator_rating, 2).unwrap();
-    let co2_int = isize::from_str_radix(co2_scrubber_rating, 2).unwrap();
+    let oxygen_int = isize::from_str_radix(oxygen_generator_rating, 2)?;
+    let co2_int = isize::from_str_radix(co2_scrubber_rating, 2)?;
     dbg!(oxygen_int, co2_int, oxygen_int * co2_int);
     Ok(())
+}
+
+fn calculate(mut input: Vec<&str>, rating_type: RatingType) -> anyhow::Result<&str> {
+    let mut count = 0;
+    while input.len() > 1 {
+        let (ones, zeroes): (Vec<_>, Vec<_>) = input.iter().partition(|line| {
+            line.chars().nth(count) == Some('1')
+        });
+        count += 1;
+        input = match rating_type {
+            OXYGEN => {
+                if ones.len() >= zeroes.len() { ones } else { zeroes }
+            }
+            CO2 => {
+                if ones.len() < zeroes.len() { ones } else { zeroes }
+            }
+        }
+    }
+    Ok(input.get(0).context("no element left")?)
 }
