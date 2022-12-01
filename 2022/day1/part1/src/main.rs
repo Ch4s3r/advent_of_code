1fec2a00e0afb15613faaf9e70fc261270d13229
Ch4s3r <lamprecht.patrick1@gmail.com>
2022-12-01 21:03:09 +0100

    Change part 1

--- 2022/day1/part1/src/main.rs
+++ 2022/day1/part1/src/main.rs
@@ -1,14 +1,13 @@
 use anyhow::{Context, Result};
 use nom::character::complete;
 use nom::character::complete::newline;
-use nom::combinator::opt;
-use nom::multi::{many0, separated_list0};
-use nom::sequence::terminated;
+use nom::multi::separated_list1;
+use nom::sequence::tuple;
 use nom::IResult;
 
 fn main() -> Result<()> {
-    let input = include_str!("../data/input_test.txt");
-    let numbers = parse_input(input)?.1;
+    let input = include_str!("../data/input.txt");
+    let numbers = dbg!(parse_input(input))?.1;
     let highest_calories = numbers
         .into_iter()
         .map(|number_block| number_block.iter().fold(0, |acc, x| acc + x))
@@ -19,9 +18,9 @@ fn main() -> Result<()> {
 }
 
 fn parse_input(input: &str) -> IResult<&str, Vec<Vec<i32>>> {
-    separated_list0(newline, parse_numbers)(input)
+    separated_list1(tuple((newline, newline)), parse_numbers)(input)
 }
 
 fn parse_numbers(input: &str) -> IResult<&str, Vec<i32>> {
-    many0(terminated(complete::i32, opt(newline)))(input)
+    separated_list1(newline, complete::i32)(input)
 }
