2c006c911e4bbd5b08b0280ea4af3ff37323445a
Ch4s3r <lamprecht.patrick1@gmail.com>
2022-12-03 12:18:38 +0100

    Part 2

---
+++ 2022/day2/part2/src/main.rs
@@ -0,0 +1,142 @@
+use std::str::FromStr;
+
+use anyhow::Result;
+use enum_iterator::{next_cycle, previous_cycle, Sequence};
+use itertools::Itertools;
+use nom::bytes::complete::tag;
+use nom::character::complete::{alpha1, newline};
+use nom::multi::separated_list1;
+use nom::sequence::tuple;
+use nom::IResult;
+
+use crate::GameOutcome::{Draw, Lose, Win};
+use crate::HandType::{Paper, Rock, Scissor};
+
+#[derive(Debug, PartialEq)]
+struct Game {
+    opponent: HandType,
+    game_outcome: GameOutcome,
+}
+
+#[derive(Debug, PartialEq, Sequence, Clone)]
+enum HandType {
+    Rock,
+    Paper,
+    Scissor,
+}
+
+impl FromStr for HandType {
+    type Err = ();
+
+    fn from_str(input: &str) -> std::result::Result<Self, Self::Err> {
+        match input {
+            "A" => Ok(Rock),
+            "B" => Ok(Paper),
+            "C" => Ok(Scissor),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum GameOutcome {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl FromStr for GameOutcome {
+    type Err = ();
+
+    fn from_str(input: &str) -> std::result::Result<Self, Self::Err> {
+        match input {
+            "X" => Ok(Lose),
+            "Y" => Ok(Draw),
+            "Z" => Ok(Win),
+            _ => Err(()),
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let input = include_str!("../data/input.txt");
+    app(input)?;
+    Ok(())
+}
+
+fn app(input: &str) -> Result<i32> {
+    let games = parse_input(input).unwrap().1;
+    let points = dbg!(games
+        .iter()
+        .map(|game| game.calculate_points())
+        .collect_vec());
+    let total_points: i32 = points.iter().sum();
+    dbg!(total_points);
+    Ok(total_points)
+}
+
+fn parse_input(input: &str) -> IResult<&str, Vec<Game>> {
+    separated_list1(newline, parse_game)(input)
+}
+
+fn parse_game(input: &str) -> IResult<&str, Game> {
+    let (input, (opponent, _, player)) = tuple((alpha1, tag(" "), alpha1))(input)?;
+    Ok((
+        input,
+        Game {
+            opponent: HandType::from_str(opponent).unwrap(),
+            game_outcome: GameOutcome::from_str(player).unwrap(),
+        },
+    ))
+}
+
+impl From<&HandType> for i32 {
+    fn from(handtype: &HandType) -> Self {
+        match handtype {
+            Rock => 1,
+            Paper => 2,
+            Scissor => 3,
+        }
+    }
+}
+
+impl Game {
+    fn calculate_points(&self) -> i32 {
+        let opponent_points = i32::from(&self.opponent);
+        let player_points = i32::from(&self.get_player_move());
+
+        if player_points == 3 && opponent_points == 1 {
+            return player_points;
+        }
+        if player_points > opponent_points || player_points == 1 && opponent_points == 3 {
+            return player_points + 6;
+        }
+        if player_points == opponent_points {
+            return player_points + 3;
+        }
+        player_points
+    }
+
+    fn get_player_move(&self) -> HandType {
+        match self.game_outcome {
+            Lose => previous_cycle(&self.opponent).unwrap(),
+            Draw => self.opponent.clone(),
+            Win => next_cycle(&self.opponent).unwrap(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() -> Result<()> {
+        let input = "A Y
+B X
+C Z";
+        let actual = app(input)?;
+        assert_eq!(12, actual);
+        Ok(())
+    }
+}
